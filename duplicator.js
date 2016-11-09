b97bf527cb6d4b5ae7993d31af11538226c883a9
Mithilesh Salunke <mithilesh.salunke@gmail.com>
2016-11-09 00:10:18 -0800

    Adding a duplicator method

--- duplicator.js
+++ duplicator.js
@@ -3,3 +3,10 @@
  * Eg: duplicator([123] give us [123,123]
  *
  */
+function ms_duplicator (arrToDuplicate) {
+  if (arrToDuplicate && Array.isArray(arrToDuplicate)) {
+    return arrToDuplicate.concat(arrToDuplicate);
+  } else {
+    console.log('Please provide a valid array to duplicate');
+  }
+}
